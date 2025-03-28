04e185223f85a4e7ff4fdd5cd0f44baf2972a1fb
10221190 <10221190@B207PC24>
2025-03-28 20:40:08 +0300

    Добавьте файлы проекта.

---
+++ WpfApp2/Loggin.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Логика взаимодействия для Loggin.xaml
+    /// </summary>
+    public partial class Loggin : Window
+    {
+        
+        
+        public Loggin()
+        {
+            InitializeComponent();
+        }
+
+        private void OnClick(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+
+            Close();
+        }
+    }
+}
